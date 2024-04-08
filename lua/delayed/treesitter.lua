00b576065d3499dcb15c602294f112cb43ae1354
Nikola Wuttke <heiligeslama@gmail.com>
2024-04-08 15:56:07 +0200

    Enable git commit diffing

--- lua/delayed/treesitter.lua
+++ lua/delayed/treesitter.lua
@@ -8,12 +8,14 @@ require("nvim-treesitter.configs").setup({
 		"vimdoc",
 		"query",
 		"python",
+		"diff",
+		"git_rebase",
 	},
 	sync_install = false,
 	auto_install = true,
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = false,
-		disable = { "gitcommit", "diff" },
+		additional_vim_regex_highlighting = true,
+		-- disable = { "gitcommit", "diff" },
 	},
 })

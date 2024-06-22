903746e9a6f06dee5a07bd71a1477260c8947bdb
Nikola Wuttke <heiligeslama@gmail.com>
2024-06-22 23:25:17 +0200

    Update lualine to show relative paths

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,15 @@ return {
 			options = {
 				theme = "auto",
 			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true,
+						path = 1,
+					},
+				},
+			},
 			extensions = {
 				"quickfix",
 				"fugitive",

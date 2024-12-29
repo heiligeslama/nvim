a2a9ad0c656ed854000e10c8f47e21a961ca0d14
Nikola Wuttke <heiligeslama@gmail.com>
2024-12-29 14:13:39 +0100

    Fixed behavior for code completion with normal mode

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,31 +5,28 @@ return {
 
         version = "*",
 
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.Config
         opts = {
-            -- 'default' for mappings similar to built-in completion
-            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-            -- See the full "keymap" documentation for information on defining your own keymap.
-            keymap = { preset = "super-tab" },
+            keymap = {
+                preset = "enter",
+                cmdline = {
+                    preset = "super-tab"
+                }
+            },
 
             appearance = {
-                -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-                -- Useful for when your theme doesn't support blink.cmp
-                -- Will be removed in a future release
                 use_nvim_cmp_as_default = true,
-                -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-                -- Adjusts spacing to ensure icons are aligned
                 nerd_font_variant = "normal",
             },
 
-            -- Default list of enabled providers defined so that you can extend it
-            -- elsewhere in your config, without redefining it, due to `opts_extend`
             sources = {
                 default = { "lsp", "path", "snippets", "buffer" },
             },
-            signature = { enabled = true }
+            signature = { enabled = true },
+            completion = {
+                ghost_text = {
+                    enabled = true
+                }
+            }
         },
         opts_extend = { "sources.default" },
     },
@@ -41,11 +38,13 @@ return {
             "williamboman/mason-lspconfig.nvim"
         },
 
-        -- example using `opts` for defining servers
         opts = {
             servers = {
                 lua_ls = {},
                 gopls = {},
+                rust_analyzer = {},
+                clangd = {},
+                basedpyright = {},
             },
         },
         config = function(_, opts)

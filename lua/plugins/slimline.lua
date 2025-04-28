fa06c4b6434f9d86be4f26a8d6f6354f2d269834
Nikola Wuttke <heiligeslama@gmail.com>
2025-04-28 17:04:23 +0200

    Add column to status line

--- lua/plugins/slimline.lua
+++ lua/plugins/slimline.lua
@@ -1,23 +1,28 @@
 return {
-	"sschleemilch/slimline.nvim",
-	dependencies = {
-		"lewis6991/gitsigns.nvim",
-		"echasnovski/mini.icons",
-	},
-	config = true,
-	opts = {
-		spaces = {
-			components = "",
-			left = "",
-			right = "",
-		},
-		sep = {
-			hide = {
-				first = true,
-				last = true,
-			},
-			left = "",
-			right = "",
-		},
-	},
+    "sschleemilch/slimline.nvim",
+    dependencies = {
+        "lewis6991/gitsigns.nvim",
+        "echasnovski/mini.icons",
+    },
+    config = true,
+    opts = {
+        spaces = {
+            components = "",
+            left = "",
+            right = "",
+        },
+        sep = {
+            hide = {
+                first = true,
+                last = true,
+            },
+            left = "",
+            right = "",
+        },
+        configs = {
+            progress = {
+                column = true,
+            }
+        }
+    },
 }
